0859cfabf45db9afaa64a33d74967767012b0fa8
cgirl <1579090779@qq.com>
2016-01-07 21:34:49 +0800

    自己动手开发模板类

---
+++ apply/003/compile/003.html.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title><?php echo $this->_tpl_var['title'];?></title>
+</head>
+<body>
+	<p><?php echo $this->_tpl_var['content'];?></p>
+</body>
+</html>
