8acc52ac2e4ca091becdd70cc4b700dde15d4be3
cgirl <1579090779@qq.com>
2016-01-17 21:42:02 +0800

    单模板多缓存

---
+++ apply/017/cache/19^7c85e9959564eb55918bcc6e7e2254d21047c0c9.017_01.html.php
@@ -0,0 +1,34 @@
+<?php
+/* Smarty version 3.1.29, created on 2016-01-17 21:31:41
+  from "F:\web\www\smarty\apply\017\template\017_01.html" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_569b97bd4de631_09881945',
+  'file_dependency' => 
+  array (
+    '7c85e9959564eb55918bcc6e7e2254d21047c0c9' => 
+    array (
+      0 => 'F:\\web\\www\\smarty\\apply\\017\\template\\017_01.html',
+      1 => 1453036138,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 3600,
+),true)) {
+function content_569b97bd4de631_09881945 ($_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>单模板多缓存</title>
+</head>
+<body>
+	<h2>实木多层 经典系列-红檀香</h2>
+	<p>265.00</p>
+	<p>19</p>
+</body>
+</html><?php }
+}
---
+++ apply/017/compile/7c85e9959564eb55918bcc6e7e2254d21047c0c9_0.file.017_01.html.cache.php
@@ -0,0 +1,40 @@
+<?php
+/* Smarty version 3.1.29, created on 2016-01-17 21:31:34
+  from "F:\web\www\smarty\apply\017\template\017_01.html" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_569b97b63bfe09_89869239',
+  'file_dependency' => 
+  array (
+    '7c85e9959564eb55918bcc6e7e2254d21047c0c9' => 
+    array (
+      0 => 'F:\\web\\www\\smarty\\apply\\017\\template\\017_01.html',
+      1 => 1453036138,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_569b97b63bfe09_89869239 ($_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '5363569b97b626c8f5_95706053';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>单模板多缓存</title>
+</head>
+<body>
+	<h2><?php echo $_smarty_tpl->tpl_vars['goods_name']->value;?>
+</h2>
+	<p><?php echo $_smarty_tpl->tpl_vars['shop_price']->value;?>
+</p>
+	<p><?php echo $_smarty_tpl->tpl_vars['goods_id']->value;?>
+</p>
+</body>
+</html><?php }
+}
